e1f93f3b9aa604c99d67da82d8c535b476600317
wamahiga <wanjirucynthia01@gmail.com>
2023-03-18 10:15:27 +0300

    useEffect cleanup

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,11 @@
+import { Link } from 'react-router-dom';
 const Navbar = () => {
     return ( 
         <nav className="navbar">
             <h1>The Dojo Blog</h1>
             <div className="links">
-                <a href="/">Home</a>
-                <a href="/create">NewBlog</a>
+                <Link to="/">Home</Link>
+                <Link to="/create">NewBlog</Link>
               
             </div>
         </nav>
