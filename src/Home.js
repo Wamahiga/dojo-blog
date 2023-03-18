1ee0da63ce2eaa96436a0d964ce02ad72dcef80d
wamahiga <wanjirucynthia01@gmail.com>
2023-03-18 16:49:14 +0300

    first ever react appliaction

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,16 @@
-import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 import useFetch from "./useFetch";
 
 const Home = () => {
-  const{data: blogs, isPending, error} = useFetch('http://localhost:8000/blogs');
-  
+  const { error, isPending, data: blogs } = useFetch('http://localhost:8000/blogs')
+
   return (
     <div className="home">
-      {error && <div>{error}</div> }
-      {isPending && <div>Loading...</div>}
-      {blogs && <BlogList blogs={blogs}  title="All Blogs!"/>}
+      { error && <div>{ error }</div> }
+      { isPending && <div>Loading...</div> }
+      { blogs && <BlogList blogs={blogs} /> }
     </div>
+    
   );
 }
  
